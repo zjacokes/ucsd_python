25981ae0d88c92832c086b526f03601d47716ac4
zjacokes <75634174+zjacokes@users.noreply.github.com>
2021-08-21 15:40:12 -0700

    Add files via upload

--- cards.py
+++ cards.py
@@ -1,4 +1,6 @@
-suit = ["Club", "Diamond", "Heart", "Spade"]
+#card class creation
+#my code runs fine without the list of values
+suit = ["Clubs", "Diamonds", "Hearts", "Spades"]
 value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 class Card:
@@ -7,15 +9,37 @@ class Card:
         self.suit = suit
         self.value = value
 
-    def get_display_string(value):
-        if value == 1:
-            return "Ace"
-        elif value == 11:
-            return "Jack"
-        elif value == 12:
-            return "Queen"
-        elif value == 13:
-            return "King"
+    def get_display_string(self):
+        if self.value == 1:
+            return "Ace of " + suit[self.suit-1]
+        elif self.value == 11:
+            return "Jack of " + suit[self.suit-1]
+        elif self.value == 12:
+            return "Queen of " + suit[self.suit-1]
+        elif self.value == 13:
+            return "King of " + suit[self.suit-1]
         else:
-            value = str(value)
-            return value
+            return str(self.value) + " of " + suit[self.suit-1]
+
+
+#####This was the first functional code I wrote, but I knew there was more I could do with my suit list so I kept plugging away at it
+#    def get_display_string(self):
+#        if self.suit == 1:
+#            self.suit = "Clubs"
+#        elif self.suit == 2:
+#            self.suit = "Diamonds"
+#        elif self.suit == 3:
+#            self.suit = "Hearts"
+#        else:
+#            self.suit = "Spades"
+#####I thought using the code below this comment would work by itself, but I kept getting numbers for self.suit instead of the names (also had to do str(self.suit) to even get it to print)
+#        if self.value == 1:
+#            return "Ace" + " of " + self.suit
+#        elif self.value == 11:
+#            return "Jack" + " of " + self.suit
+#        elif self.value == 12:
+#            return "Queen" + " of " + self.suit
+#        elif self.value == 13:
+#            return "King" + " of " + self.suit
+#        else:
+#            return str(self.value) + " of " + self.suit
